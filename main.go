79e1fe76f4befa8d4d96b1c77631dfed4cc2aac7
Miguel Laignier <miguelrgit@gmail.com>
2025-04-07 15:20:41 -0300

    Rotas iniciais criadas

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 
@@ -18,7 +19,11 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
+	e.GET("/students", getStudents)
+	e.POST("/students", createStudent)
+	e.GET("/students/:id", getStudent)
+	e.PUT("/students/:id", updateStudent)
+	e.DELETE("/students/:id", deleteStudent)
 
 	// Start server
 	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -27,6 +32,24 @@ func main() {
 }
 
 // Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func getStudents(c echo.Context) error {
+	return c.String(http.StatusOK, "List of all students")
+}
+func createStudent(c echo.Context) error {
+	return c.String(http.StatusOK, "Create student")
+}
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	getStud := fmt.Sprintf("Get %s student:", id)
+	return c.String(http.StatusOK, getStud)
+}
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Update %s student:", id)
+	return c.String(http.StatusOK, updateStud)
+}
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	deleteStud := fmt.Sprintf("Delete %s student:", id)
+	return c.String(http.StatusOK, deleteStud)
 }
