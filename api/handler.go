12be3858a07e328a243283533b4f6bf003c4a414
Miguel Laignier <miguelrgit@gmail.com>
2025-05-07 12:19:28 -0300

    Validação de criação de um student

--- api/handler.go
+++ api/handler.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/miguellaig/api-students2/schemas"
+	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
@@ -20,10 +21,24 @@ func (api *API) getStudents(c echo.Context) error {
 }
 
 func (api *API) createStudent(c echo.Context) error {
-	student := schemas.Student{}
-	if err := c.Bind(&student); err != nil {
+	studentReq := StudentRequest{}
+	if err := c.Bind(&studentReq); err != nil {
 		return err
 	}
+
+	if err := studentReq.Validate(); err != nil {
+		log.Error().Err(err).Msgf("[api] error validating struct")
+		return c.String(http.StatusBadRequest, "Error validating student")
+	}
+
+	student := schemas.Student{
+		Name:   studentReq.Name,
+		CPF:    studentReq.CPF,
+		Email:  studentReq.Email,
+		Age:    studentReq.Age,
+		Active: *studentReq.Active,
+	}
+
 	if err := api.DB.AddStudent(student); err != nil {
 		return c.String(http.StatusInternalServerError, "Error to create student")
 	}
---
+++ api/request.go
@@ -0,0 +1,34 @@
+package api
+
+import "fmt"
+
+type StudentRequest struct {
+	Name   string `json:"name"`
+	CPF    int    `json:"cpf"`
+	Email  string `json:"email"`
+	Age    int    `json:"age"`
+	Active *bool  `json:"active"` //using bool as a pointer to force true/false input
+}
+
+func errParamRequired(param, typ string) error {
+	return fmt.Errorf("param '%s' of type '%s' is required", param, typ)
+}
+
+func (s *StudentRequest) Validate() error {
+	if s.Name == "" {
+		return errParamRequired("name", "string")
+	}
+	if s.CPF == 0 {
+		return errParamRequired("cpf", "int")
+	}
+	if s.Email == "" {
+		return errParamRequired("email", "string")
+	}
+	if s.Age == 0 {
+		return errParamRequired("age", "int")
+	}
+	if s.Active == nil {
+		return errParamRequired("active", "bool")
+	}
+	return nil
+}
